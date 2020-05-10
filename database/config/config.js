063ec7eecfcaf7f111cce44aedafd6bcfa8f5331
Andres Suarez <andresfelipe9619@gmail.com>
2020-05-10 08:20:45 -0500

    :alien: Use mysql instead of postgres

--- database/config/config.js
+++ database/config/config.js
@@ -3,14 +3,14 @@ require('dotenv').config()
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialect: 'postgres',
+    dialect: 'mysql',
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    dialect: 'postgres',
+    dialect: 'mysql',
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres',
+    dialect: 'mysql',
   },
 }

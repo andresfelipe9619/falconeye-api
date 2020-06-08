d0047d3947bf76f9938da723458ad9be1cd19f74
Andres Suarez <andresfelipe9619@gmail.com>
2020-06-07 20:25:55 -0500

    :art: Improve npm scripts and seeders to be executed only once

--- database/config/config.js
+++ database/config/config.js
@@ -4,14 +4,17 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     logging: false,
-    dialect: "mysql"
+    dialect: "mysql",
+    seederStorage: "sequelize"
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    dialect: "mysql"
+    dialect: "mysql",
+    seederStorage: "sequelize"
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: "mysql"
+    dialect: "mysql",
+    seederStorage: "sequelize"
   }
 }
